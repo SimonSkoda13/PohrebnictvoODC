fb2d018ce7d41f9b8188bf1cf841f275d16f719e
SimonSkoda13 <simonskoda1354@gmail.com>
2022-02-13 21:54:33 +0100

    chore:WhyUs

--- src/components/Sections/WhyUs/WhyUs.tsx
+++ src/components/Sections/WhyUs/WhyUs.tsx
@@ -14,11 +14,17 @@ export const WhyUs = (props: WhyUsProps) => {
   const { title, description, items, ...restOfProps } = props
   return (
     <Container py={10}>
-      <SectionHeading title={title} description={description} isCentered />
+      <SectionHeading
+        title={title}
+        description={description}
+        isCentered
+        py={5}
+      />
       <Flex
         flexDir={{ md: "row", base: "column" }}
         alignItems={{ md: "normal", base: "center" }}
         justifyContent={"center"}
+        py={5}
       >
         {items.map(item => {
           return <WhyUsItem {...item} />
